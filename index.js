a398971a8e8ef5d038e23e20e52b22415f1dc34c
kamalsirssjcoe <140489074+kamalsirssjcoe@users.noreply.github.com>
2024-01-19 11:24:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const con = mysql.createConnection({
+	"host" : "sql12.freesqldatabase.com",
+	"user" : "sql12678040",
+	"password" : "txRlRB32hL",
+	"database" : "sql12678040"
+});
+
+app.post("/save", (req, res) => {
+	let data = [req.body.name, req.body.feedback];
+	let sql  = "insert into student values(?, ?)";
+	con.query(sql, data, (err, result) => {
+		if (err)		res.send(err);
+		else			res.send(result);
+	});
+});
+
+app.listen(9000, () => { console.log("ready to server @ 9000 ")} )
+
+
+
+
+
+
+
+
+
+
+
